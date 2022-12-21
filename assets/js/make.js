601924eccf365f8aaceb8bbfb75aea784ecca7c7
malcolm.wilkie <malcolm.wilkie@and.digital>
2022-12-21 10:35:42 +0000

    AWS S3 integration

--- assets/js/make.js
+++ assets/js/make.js
@@ -17,7 +17,7 @@ function loadImages () {
   $.each(toppings, function (key, val) {
     const img = new window.Image()
     img.setAttribute('crossOrigin', 'anonymous')
-    img.src = '/assets/toppings/' + val.image
+    img.src = '//pizza-lovers-bucket.s3.eu-west-2.amazonaws.com/toppings/' + val.image
     val.img = img
   })
 }
